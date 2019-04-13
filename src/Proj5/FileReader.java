a36b734f1b4477d90a94961f7219fdd4133b286c
Riyadn99 <riyadn99@vt.edu>
2019-04-13 16:18:27 -0400

    The second method

--- src/Proj5/FileReader.java
+++ src/Proj5/FileReader.java
@@ -10,10 +10,10 @@
 package Proj5;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
-import spacecolonies.ArrayQueue;
-import spacecolonies.Person;
 
 /**
  * Add the class description here.
@@ -24,7 +24,7 @@ import spacecolonies.Person;
  */
 public class FileReader<E> {
     private LList<E> songs;
-    private ArrayQueue<People> peopleQueue;
+    private ArrayList<People> peopleList;
     private SongSorter songSort;
 
 
@@ -32,35 +32,107 @@ public class FileReader<E> {
      * 
      */
     public FileReader(String applicantFile, String songFile) {
-        peopleQueue = this.readSurveyFile(applicantFile);
-        songSort = this.readMusicFile(songFile);
-        songSort = new SongSorter(peopleQueue, songs);
-        new GUIMusicWindow(songSorter);
+// peopleQueue = this.readSurveyFile(applicantFile);
+// songSort = this.readMusicFile(songFile);
+// songSort = new SongSorter(peopleQueue, songs);
+// new GUIMusicWindow(songSorter);
     }
 
 
-    public LList<Songs> readMusicFile(String fileName){
-        Scanner scan = new Scanner(new File(fileName);
-        
-        
-        
+    public LList<Song> readMusicFile(String fileName)
+        throws FileNotFoundException {
+        Scanner scan = new Scanner(new File(fileName));
+        return null;
+
     }
 
 
-    public ArrayQueue<People> readSurveyFile(String names) {
+    public ArrayList<People> readSurveyFile(String names)
+        throws FileNotFoundException {
 
         Scanner scan = new Scanner(new File(names));
-        ArrayQueue<People> queue = new ArrayQueue<People>();
+        ArrayList<People> people = new ArrayList<People>();
 
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
-            String[] split = line.split(", *", -1);
-            People p = new People((split[4]),(
-                split[5]), (split[6]), ); //how to get the array of yes/no? 
-            
+
+            String[] blockSplit = line.split(", *", -1);
+            String[] resp = null;
+
+            int count = 0;
+            boolean make = false;
+            MajorEnum majoring = null;
+            RegionEnum reg = null;
+            HobbyEnum hobby = null;
+
+            switch (blockSplit[2]) {
+                case "Math or CMDA":
+                    majoring = MajorEnum.MATHCMDA;
+                    break;
+                case "Computer Science":
+                    majoring = MajorEnum.COMPSCI;
+                    break;
+                case "Other Engineering":
+                    majoring = MajorEnum.OTHERENG;
+                    break;
+                case "Other":
+                    majoring = MajorEnum.OTHER;
+                    break;
+                default:
+                    make = true;
+                    break;
+            }
+            switch (blockSplit[3]) {
+                case "Southeast":
+                    reg = RegionEnum.SE_US;
+                    break;
+                case "Northeast":
+                    reg = RegionEnum.NE_US;
+                    break;
+                case "United States (other than Southeast or Northwest)":
+                    reg = RegionEnum.OTHER_US;
+                    break;
+                case "Outside of United States":
+                    reg = RegionEnum.OUTSIDE_US;
+                    break;
+                default:
+                    make = true;
+                    break;
+            }
+
+            switch (blockSplit[4]) {
+                case "sports":
+                    hobby = HobbyEnum.SPORTS;
+                    break;
+                case "music":
+                    hobby = HobbyEnum.MUSIC;
+                    break;
+                case "reading":
+                    hobby = HobbyEnum.READ;
+                    break;
+                case "art":
+                    hobby = HobbyEnum.ART;
+                    break;
+                default:
+                    make = true;
+                    break;
+            }
+
+            for (int i = 0; i < blockSplit.length; i++) {
+                if (blockSplit[i].equals("yes") || blockSplit[i].equals("no")) {
+                    resp[count] = blockSplit[i];
+                    count++;
+
+                }
+
+            }
+            People p = new People(hobby, majoring, reg, resp);
+            people.add(p);
+
         }
         scan.close();
-        return queue;
+
+        return people;
 
     }
 
